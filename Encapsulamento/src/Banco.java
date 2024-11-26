ce1a29e83d4626503fdd0f7cb62a4f389f912267
Juan Santos do Nascimento <njuan6799@gmail.com>
2024-11-26 13:33:13 -0300

    Update Banco.java

--- Encapsulamento/src/Banco.java
+++ Encapsulamento/src/Banco.java
@@ -12,6 +12,12 @@
 public class Banco {
     
     
+    public void menu(){
+        
+        
+    }
+    
+    
     public static void main(String[] args) {
         Conta cc = new Conta("Juan", 2500d, 1234);
         
@@ -19,8 +25,41 @@ public class Banco {
         //cc.realizarSaque();
         //cc.trocarSenha();
         //cc.exibirDadosconta();
-        cc.exibirSaldo();
+        //cc.exibirSaldo();
+        boolean sair = false;
         
+        while(!sair){
+            String opcao = JOptionPane.showInputDialog(
+            "Escolha uma operação:\n" +
+            "1 - Realizar Saque\n" +
+            "2 - Realizar Depósito\n" +
+            "3 - Exibir Saldo\n" +
+            "4 - Trocar Senha\n" +
+            "5 - Sair");
+            
+           switch (opcao){
+               case "1":
+                   cc.realizarSaque();
+                   break;
+               case "2":
+                   cc.realizarDeposito();
+                   break;
+               case "3":
+                   cc.exibirSaldo();
+                   break;
+               case "4":
+                   cc.trocarSenha();
+                   break;
+               case "5":
+                   sair = true;
+                   JOptionPane.showMessageDialog(null, "Saindo...");
+                   break;
+               default:
+                   JOptionPane.showMessageDialog(null, "Opção inválida!");
+           }
+            
+        }
+        cc.exibirDadosconta();
         
     }
 
